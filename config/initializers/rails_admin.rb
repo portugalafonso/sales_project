a162b09680e850f7a95e5e417c76f92bee7ce736
Paulo Batista <paulocsb@gmail.com>
2021-05-20 12:02:28 -0700

    Fix cancan ability and rails admin config for cancancan

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,10 +6,11 @@ RailsAdmin.config do |config|
   config.authenticate_with do
     warden.authenticate! scope: :user
   end
+
   config.current_user_method(&:current_user)
 
   # == CancanCan ==
-  config.authorize_with :cancan
+  config.authorize_with :cancancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
